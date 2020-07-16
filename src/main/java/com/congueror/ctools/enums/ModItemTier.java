0e3a8f588998a2093e7eadc613acad4d36acd5e1
congueror <38083828+congueror@users.noreply.github.com>
2020-07-16 03:49:39 +0300

    Error

---
+++ src/main/java/com/congueror/ctools/enums/ModItemTier.java
@@ -0,0 +1,64 @@
+package com.congueror.ctools.enums;
+
+import java.util.function.Supplier;
+
+import com.congueror.clib.init.ItemInit;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+public enum ModItemTier implements IItemTier
+{
+	//HarvestLvl, Durability, Efficiency(Wood = 2.0, Diamond = 10.0), Attack Damage(Wood = 0), Enchantibility(Wood = 15), RepairMat
+	TIN(1, 31, 2.5f, 1.1f, 9, () -> {
+		return Ingredient.fromItems(ItemInit.TIN_INGOT.get());
+	});
+	
+	private final int harvestLevel;
+	private final int maxUses;
+	private final float efficiency;
+	private final float attackDamage;
+	private final int enchantibility;
+	private final LazyValue<Ingredient> repairMaterial;
+	
+	private ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantibility, Supplier<Ingredient> repairMaterial) 
+	{
+		this.harvestLevel = harvestLevel;
+		this.maxUses = maxUses;
+		this.efficiency = efficiency;
+		this.attackDamage = attackDamage;
+		this.enchantibility = enchantibility;
+		this.repairMaterial = new LazyValue<>(repairMaterial);
+	}
+
+	@Override
+	public int getMaxUses() {
+		return this.maxUses;
+	}
+
+	@Override
+	public float getEfficiency() {
+		return this.efficiency;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return this.attackDamage;
+	}
+
+	@Override
+	public int getHarvestLevel() {
+		return this.harvestLevel;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return this.enchantibility;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.getValue();
+	}
+}
--- src/main/java/com/congueror/ctools/init/ItemInit.java
+++ src/main/java/com/congueror/ctools/init/ItemInit.java
@@ -1,13 +1,18 @@
 package com.congueror.ctools.init;
 
 import com.congueror.ctools.ConguerorTools;
+import com.congueror.ctools.CtoolsItemGroup;
+import com.congueror.ctools.enums.ModItemTier;
+
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemInit 
 {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ConguerorTools.MOD_ID);
-    
-
+	
+    public static final RegistryObject<Item> TIN_SWORD = ITEMS.register("tin_sword", ()-> new SwordItem(ModItemTier.TIN, 1, 0.7f, new Item.Properties().group(CtoolsItemGroup.instance)));
 }
